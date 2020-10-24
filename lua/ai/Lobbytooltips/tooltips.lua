ccf94168992d08eb7cfe94fcd52404685d9991dc
muchstuffmuchstuff <73402983+muchstuffmuchstuff@users.noreply.github.com>
2020-10-24 15:20:26 -0400

    Create tooltips.lua

---
+++ lua/ai/Lobbytooltips/tooltips.lua
@@ -0,0 +1,12 @@
+
+
+Tooltips = {
+    aitype_nut_cracker = {
+        title = "<LOC Nut_Cracker_0001>AI: Nut Cracker",
+        description = "<LOC Nut_Cracker_0002>Designed to beat turtle humans on large maps",
+    },
+    aitype_microaicheat = {
+        title = "<LOC Nut_Cracker_0003>AIx: Nut Cracker",
+        description = "<LOC Nut_Cracker_0004>Same as non-cheating, but even harder",
+    },
+}
