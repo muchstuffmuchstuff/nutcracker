0669af581a810223f7cbed2165a8cc2842043709
muchstuffmuchstuff <73402983+muchstuffmuchstuff@users.noreply.github.com>
2020-10-24 15:18:13 -0400

    Create nut_cracker.lua

---
+++ lua/ai/CustomAIs_v2/nut_cracker.lua
@@ -0,0 +1,24 @@
+--[[
+    File    :   /lua/AI/CustomAIs_v2/nut_cracker.lua
+    Author  :   SoftNoob
+    Summary :
+        Lists AIs to be included into the lobby, see /lua/AI/CustomAIs_v2/SorianAI.lua for another example.
+        Loaded in by /lua/ui/lobby/aitypes.lua, this loads all lua files in /lua/AI/CustomAIs_v2/
+]]
+
+AI = {
+	Name = 'Nut Cracker',
+	Version = '1',
+	AIList = {
+		{
+			key = 'nut_cracker',
+			name = '<LOC Nut_Cracker_0001>AI: Nut Cracker',
+		},
+	},
+	CheatAIList = {
+		{
+			key = 'nut_crackercheat',
+			name = '<LOC Nut_Cracker_0003>AIx: Nut Cracker',
+		},
+	},
+}
