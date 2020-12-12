80995820515e8eecd6638ba7dd670c37d5a628c0
muchstuffmuchstuff <73402983+muchstuffmuchstuff@users.noreply.github.com>
2020-12-11 22:18:31 -0500

    Update mod_info.lua

--- mod_info.lua
+++ mod_info.lua
@@ -3,10 +3,12 @@ name = "NutCracker"
 uid = "278ce3a8-1608-11eb-adc1-0242ac120002"
 version = 1
 copyright = "FreeWare"
-description = "Designed to beat Turtle Human Players on large maps"
+description = "Designed to battle against Turtle Human Players on 20x20+ maps. Hundreds of hours of testing has been focused solely on large maps, to ensure it is capable and tuned to the challenges faced on that map size.
+It is not tested or designed for small maps (10x10 and 5x5) and will not perform well. To ensure the best playing experience, ensure you activate this AI and Uveso so that you can enjoy it as I've intended, and as I hope you all do.
+You can find me on https://discord.gg/jW5HV8sw if you wish to chat, or if you see a majar flaw. I hope you enjoy it, I've enjoyed making it."
 author = "muchstuff"
 
-icon = "/mods/nut_cracker/nutcracker.jpg"
+icon = "/mods/nutcracker/nutcracker.jpg"
 selectable = true
 enabled = true
 exclusive = false
